008e684afd2d08db14b446abacef8101dfb2ab42
Bostjan Oblak <bostjan@muha.cc>
2020-01-02 17:58:19 +0100

    console command, github actions test

---
+++ app/Console/Commands/CheckersRun.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Checker;
+use App\Jobs\CheckChecker;
+use Illuminate\Console\Command;
+
+class CheckersRun extends Command
+{
+    protected $signature = 'checkers:run';
+    protected $description = 'Run scheduled checkers';
+
+    public function handle()
+    {
+        Checker::where('next_run', '<', now())
+            ->each(fn(Checker $checker) => CheckChecker::dispatch($checker));
+    }
+}
---
+++ tests/Unit/Console/CheckersRunTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit\Console;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheckersRunTest extends TestCase
+{
+    use RefreshDatabase;
+}
